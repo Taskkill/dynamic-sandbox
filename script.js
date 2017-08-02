e1c7bbd729331274dd3460afdcb8a07ff192e5d5
Taskkill <johny95wil@gmail.com>
2017-08-02 16:27:34 +0200

    update

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // user defined function
-function run() {
+function ev() {
   const output = document.getElementById('console-output')
   output.innerHTML = ''
 
   const source = document.getElementById('snippet').textContent
   try {
     evaluate(
-      source,
-      {
+      source, {
         alert: val => alert(val),
         console: {
           log() {
@@ -27,6 +26,39 @@ function run() {
       },
       window
     )
+  } catch (Ex) {
+    output.innerHTML += `<span style="color:red; font-weight:bold;">${Ex} </span><br>`
+  }
+}
+
+
+function is() {
+  const output = document.getElementById('console-output')
+  output.innerHTML = ''
+
+  const source = document.getElementById('snippet').textContent
+  try {
+    isolate(
+      source,
+      {},
+      {
+        alert: val => alert(val),
+        console: {
+          log() {
+            for (let line of arguments) {
+              output.innerHTML += line + '<br>'
+            }
+          },
+          error() {
+            for (let line of arguments) {
+              output.innerHTML += `<span style="color:red">${line} </span><br>`
+            }
+          },
+          dir(object) {
+            console.dir(object)
+          }
+        }
+      })
   } catch (Ex) {
     output.innerHTML += `<span style="color:red; font-weight:bold;">${Ex} </span><br>`
   }
