7d57400e5ed9be049528fefb354756ee7061b815
Taskkill <johny95wil@gmail.com>
2017-08-05 15:51:41 +0200

    update demo to last version

--- module/main.js
+++ module/main.js
@@ -29,60 +29,78 @@
   window.createTerminal = exports.createTerminal
 
   function runInSandbox(source, context, restricted) {
-    const scope = new Proxy({
-      source,
-      context,
-      restricted,
-      Proxy,
-      eval,
-    }, {
-      has(target, propName) {
-        if (propName in target) {
+    const scope = new Proxy(
+      {
+        source,
+        context,
+        restricted,
+        Proxy,
+        eval
+      },
+      {
+        has(target, propName) {
+          if (propName in target) {
+            return false
+          }
+
+          if (propName in restricted) {
+            throw `ReferenceError: ${propName} is restricted`
+          }
+
           return false
         }
+      }
+    )
 
-        if (propName in restricted) {
-          throw `ReferenceError: ${propName} is restricted`
+    with (scope) {
+      function runInInnerSandbox(source, context) {
+        const used = {
+          eval: false,
+          source: false
         }
 
-        return false
-      },
-    })
+        const innerScope = new Proxy(
+          {
+            source,
+            eval
+          },
+          {
+            has(target, propName) {
+              if (propName in target) {
+                if (!used[propName]) {
+                  used[propName] = true
+                  return false
+                }
+                if (propName in restricted) {
+                  throw `ReferenceError: ${propName} is restricted`
+                }
+                return false
+              }
 
-    with(scope) {
-      function runInInnerSandbox(source, context) {
-        const innerScope = new Proxy({
-          source,
-          eval
-        }, {
-          has(target, propName) {
-            if (propName in target) {
+              if (propName in context) {
+                return true
+              }
+
               return false
-            }
-
-            if (propName in context) {
+            },
+            set(target, propName, value) {
+              if (context[propName]) {
+                context[propName] = value
+              } else {
+                target[propName] = value
+              }
               return true
+            },
+            get(target, propName) {
+              if (context[propName]) {
+                return context[propName]
+              }
+              return target[propName]
             }
-
-            return false
-          },
-          set(target, propName, value) {
-            if (context[propName]) {
-              context[propName] = value
-            } else {
-              target[propName] = value
-            }
-            return true
-          },
-          get(target, propName) {
-            if (context[propName]) {
-              return context[propName]
-            }
-            return target[propName]
           }
-        })
+        )
 
-        with(innerScope) {
+        with (innerScope) {
           eval(source)
         }
       }
@@ -95,60 +113,78 @@
   }
 
   function runInIsolation(source, allowed, context) {
-    const scope = new Proxy({
-      source,
-      context,
-      allowed,
-      Proxy,
-      eval,
-    }, {
-      has(target, propName) {
-        if (propName in target) {
+    const scope = new Proxy(
+      {
+        source,
+        context,
+        allowed,
+        Proxy,
+        eval
+      },
+      {
+        has(target, propName) {
+          if (propName in target) {
+            return false
+          }
+
+          if (!(propName in allowed)) {
+            throw `ReferenceError: ${propName} is restricted`
+          }
+
           return false
         }
+      }
+    )
 
-        if (!(propName in allowed)) {
-          throw `ReferenceError: ${propName} is restricted`
+    with (scope) {
+      function runInInnerIsolation(source, context) {
+        const used = {
+          eval: false,
+          source: false
         }
 
-        return false
-      },
-    })
+        const innerScope = new Proxy(
+          {
+            source,
+            eval
+          },
+          {
+            has(target, propName) {
+              if (propName in target) {
+                if (!used[propName]) {
+                  used[propName] = true
+                  return false
+                }
+                if (!(propName in allowed) && !(propName in context)) {
+                  throw `ReferenceError: ${propName} is restricted`
+                }
+                return false
+              }
 
-    with(scope) {
-      function runInInnerIsolation(source, context) {
-        const innerScope = new Proxy({
-          source,
-          eval
-        }, {
-          has(target, propName) {
-            if (propName in target) {
+              if (propName in context) {
+                return true
+              }
+
               return false
-            }
-
-            if (propName in context) {
+            },
+            set(target, propName, value) {
+              if (context[propName]) {
+                context[propName] = value
+              } else {
+                target[propName] = value
+              }
               return true
+            },
+            get(target, propName) {
+              if (context[propName]) {
+                return context[propName]
+              }
+              return target[propName]
             }
-
-            return false
-          },
-          set(target, propName, value) {
-            if (context[propName]) {
-              context[propName] = value
-            } else {
-              target[propName] = value
-            }
-            return true
-          },
-          get(target, propName) {
-            if (context[propName]) {
-              return context[propName]
-            }
-            return target[propName]
           }
-        })
+        )
 
-        with(innerScope) {
+        with (innerScope) {
           eval(source)
         }
       }
@@ -157,6 +193,47 @@
         return runInInnerIsolation(source, context)
       }
       return runInInnerIsolation.call({}, source, context)
+    }
+  }
+
+  function* runSandboxTerminal(source, context, restricted) {
+    const target = {
+      source,
+      eval
+    }
+
+    const scope = new Proxy(target, {
+      has(target, propName) {
+        if (propName in target) {
+          return true
+        }
+
+        if (propName in context) {
+          return true
+        }
+
+        if (propName in restricted) {
+          throw `ReferenceError: ${propName} is restricted`
+        }
+
+        return false
+      },
+      set(target, propName, value) {
+        if (context[propName]) context[propName] = value
+        else target[propName] = value
+        return true
+      },
+      get(target, propName) {
+        if (context[propName]) return context[propName]
+        return target[propName]
+      }
+    })
+
+    while (true) {
+      with (scope) {
+        eval(source)
+      }
+      target.source = yield
     }
   }
 })()
